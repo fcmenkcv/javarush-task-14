68344406c68c58f37a1265b5b2be1d702e032746
fcmenkcv <fcmenkcv@gmail.com>
2024-08-04 21:48:31 +0300

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task02.task0206;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        // напишите тут ваш код
+        String word = "Book";
+        String phrase = "Have a nice day";
+        String line = "________";
+        String text = "some random text";
+    }
+}
